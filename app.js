37a2af339efeec8f335afae8b48b61030b2bec16
Sebastian Maliwiecki <60139438+SebastianMaliwiecki@users.noreply.github.com>
2021-08-16 01:11:33 +0100

    Weather API added

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-const fetchLatLng = async (location) => {
+const fetchLatLng = async location => {
     try {
         const locationPromise = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=2cd3601c75784a65870073e5211ffcc5`);
         const locationResult = await locationPromise.json();
-        console.log(locationResult);
+        return  {
+            lat: locationResult.results[0].geometry.lat,
+            lng: locationResult.results[0].geometry.lng,
+        }
     } 
     catch (error) {
         console.log(error);
     }
 }
 
-document.getElementById("getWeather").addEventListener("click", () => {
+const fetchWeather = async (lat, lng) => {
+    try {
+        const weatherPromise = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=7248eff058ba1f79c53e30c2ba0fac6e`);
+        const weatherResult = await weatherPromise.json();
+        console.log(weatherResult);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const updateWeatherInfo = weatherInfo => {
+    document.getElementById("temperature").innerHTML = `Current temperature in `
+}
+
+document.getElementById("getWeather").addEventListener("click", async () => {
     console.log("Button has been pressed");
-    fetchLatLng("45645645");
+    let location = document.getElementById("userLocation").value;
+    if(location ==="")   return
+    console.log("now we fetch")
+    document.getElementById("getWeather").disabled = true;
+    const coordinates = await fetchLatLng();
+    console.log(coordinates);
+    fetchWeather(coordinates.lat, coordinates.lng);
+    document.getElementById("getWeather").disabled = false;
 })
