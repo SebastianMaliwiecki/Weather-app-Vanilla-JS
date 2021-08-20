ca6f8f9f9ef4b7b1176691cd7e8ca861aa54564c
Sebastian Maliwiecki <60139438+SebastianMaliwiecki@users.noreply.github.com>
2021-08-20 20:58:33 +0100

    unix time converter added

--- app.js
+++ app.js
@@ -16,25 +16,43 @@ const fetchWeather = async (lat, lng) => {
     try {
         const weatherPromise = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=7248eff058ba1f79c53e30c2ba0fac6e`);
         const weatherResult = await weatherPromise.json();
-        console.log(weatherResult);
+        return weatherResult;
     }
     catch (error) {
         console.log(error);
     }
 }
 
+const unixConverter = async (Utimestamp) => {
+    try {
+        const timePromise = await fetch(`https://showcase.api.linx.twenty57.net/UnixTime/fromunix?timestamp=${Utimestamp}`);
+        const timeResult = await timePromise.json();
+        return timeResult;
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
 const updateWeatherInfo = weatherInfo => {
-    document.getElementById("temperature").innerHTML = `Current temperature in `
+    document.getElementById("temperature").innerHTML = `Current temperature is ${weatherInfo.current.temp}`;
+    console.log("weather info updated.");
 }
 
 document.getElementById("getWeather").addEventListener("click", async () => {
     console.log("Button has been pressed");
-    let location = document.getElementById("userLocation").value;
+    let location = document.getElementById("userInput").value;
     if(location ==="")   return
     console.log("now we fetch")
+    // disables button
     document.getElementById("getWeather").disabled = true;
+    document.getElementById("getWeather").innerHTML = "Loading ..."
     const coordinates = await fetchLatLng(location);
     console.log(coordinates);
-    fetchWeather(coordinates.lat, coordinates.lng);
+    const weatherInfo = await fetchWeather(coordinates.lat, coordinates.lng);
+    console.log(weatherInfo)
+    updateWeatherInfo(weatherInfo); 
+    // un-disables button
     document.getElementById("getWeather").disabled = false;
+    document.getElementById("getWeather").innerHTML = "Get Weather!"
 })
